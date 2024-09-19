26ce78739f5ae712ed379cded8cce3fdfa3e0277
p23zadroznyw <p23zadroznyw@zsi.kielce.pl>
2024-09-19 17:16:06 +0200

    updated miesiac.php

---
+++ miesiac.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        h1 {
+
+        }
+    </style>
+
+</head>
+<body>
+    <?php
+        $miesiac = rand(1, 15);
+    ?>
+
+    <h1>Miesiac <?=$miesiac?></h1>
+    
+    <h3>
+        <?php
+        $kwartal =  ($miesiac-1) / 3;
+        switch (floor($kwartal) + 1) {
+            case 1:
+                echo "I kwartal";                    
+                break;
+            case 2:
+                echo "II kwartal";
+                break;
+            case 3:
+                echo "III kwartal";
+                break;
+            case 4:
+                echo "IV kwartal";
+                break;
+            default:
+                echo "Bledny numer miesiaca";
+                break;            
+        } 
+        ?>
+    </h3>
+</body>
+</html>
