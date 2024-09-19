801e957fa7676117ff9e393e6f8a7d8e069fed2c
p23zadroznyw <p23zadroznyw@zsi.kielce.pl>
2024-09-19 16:56:27 +0200

    Zadanie zrobione

---
+++ liczby.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $a = 5; $b = 15; $c = 4; $d = 35;
+    ?>
+
+    Wynik dodawania <?="$a i $b"?> wynosi <?=$a + $b?><br>
+    Wynik odejmowania <?="$a i $b"?> wynosi <?=$a - $b?><br>
+    Wynik dzielenia <?="$a i $b"?> wynosi <?=$a / $b?><br>
+    Wynik reszty z dzielenia <?="$a i $b"?> wynosi <?=$a % $b?><br>
+    <hr>
+    Obwod prostokata o bokach dlugosci <?="$c i $d"?> wynosi <?=$c*2 + $d*2?><br>
+    Pole prostokata o bokach dlugosci <?="$c i $d"?> wynosi <?=$c * $d?>j<sup>2</sup><br>
+
+    
+</body>
+</html>
