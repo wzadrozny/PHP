07968235879991ca96160966c18382d33d1bd73c
p23zadronyw <p23zadronyw@zsi.kielce.pl>
2024-09-27 13:53:26 +0200

    Zad

---
+++ Piateczek/piateczek.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .white {
+            background-color: white;
+        }
+        .cyan {
+            background-color: cyan;
+        }
+        th {
+            padding: 10px;
+        }
+        table, th, td {
+            border-collapse: collapse;
+            border: 2px solid black;
+        }
+    </style>
+
+
+</head>
+<body>
+    <table>
+        <tr>
+            <?php
+                function create($text, $bg) {
+                    echo "<th class='$bg'>$text</th>";
+                }
+                
+                $week_days = ["pon", "wto", "śro", "czw", "pią", "sob", "nie"];
+                $colors = ["white", "cyan"];
+
+                for ($i = 0; $i < 7; $i++) {
+                    create($week_days[$i], $colors[$i % 2]);
+                }
+            ?>
+        </tr>
+    </table>
+</body>
+</html>
