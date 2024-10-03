625c3cc431e9d5f5078041c0ff798fb94e543e99
p23zadroznyw <p23zadroznyw@zsi.kielce.pl>
2024-10-03 15:56:07 +0200

    a

--- Piateczek/piateczek.php
+++ Piateczek/piateczek.php
@@ -27,15 +27,14 @@
     <table>
         <tr>
             <?php
-                function create($text, $bg) {
-                    echo "<th class='$bg'>$text</th>";
-                }
-                
                 $week_days = ["pon", "wto", "śro", "czw", "pią", "sob", "nie"];
                 $colors = ["white", "cyan"];
 
                 for ($i = 0; $i < 7; $i++) {
-                    create($week_days[$i], $colors[$i % 2]);
+                    $color = $colors[$i % 2];
+                    $day = $week_days[$i];
+
+                    echo "<th class='$color'>$day</th>";
                 }
             ?>
         </tr>
