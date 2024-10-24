51ba5fb7d0725cb8c9e86f15bf772bb20c3ef768
p23zadroznyw <p23zadroznyw@zsi.kielce.pl>
2024-10-24 16:49:38 +0200

    aaa

--- Klasy/klasy.php
+++ Klasy/klasy.php
@@ -3,30 +3,50 @@
     public $n1;
     public $n2;
 
+    function are_valid_inputs() {
+        if (is_numeric($this->n1) && is_numeric($this->n2)) {
+            return true;
+        }
+        return false;
+    }
+
     function add() {
+        if (!$this->are_valid_inputs()) {
+            return null;
+        }
         return $this->n1 + $this->n2;
     }
 
     function subtract() {
+        if (!$this->are_valid_inputs()) {
+            return null;
+        }
         return $this->n1 - $this->n2;
     }
 
     function multiply() {
+        if (!$this->are_valid_inputs()) {
+            return null;
+        }
         return $this->n1 * $this->n2;
     }
 
     function divide() {
+        if (!$this->are_valid_inputs()) {
+            return null;
+        }
         if ($this->n2 == 0) {
-            echo "Nie mozna dzielic przez 0!";
-            return 0;
+            return null;
         }
 
         return $this->n1 / $this->n2;
     }
 
     function modulo() {
+        if (!$this->are_valid_inputs()) {
+            return null;
+        }
         if ($this->n2 == 0) {
-            echo "Nie mozna dzielic przez 0!<br>";
             return null;
         }
 
@@ -48,12 +68,16 @@
         $obj = new Kalkulator();
 
         $obj->n1 = 25;
-        $obj->n2 = 10;
+        $obj->n2 = 12;
+
+        $wynik = $obj->modulo();
+
+        if ($wynik == null) {
+            echo "Wprowadzono nieprawidlowe liczby!";
+            return 0;
+        }   
 
         echo "Reszta z dzielenia liczb ".$obj->n1." i ".$obj->n2." jest równa ".$obj->modulo();
-
-
-
     ?>
 </body>
 </html>
