5c29daa31d7c11f6dddb949c8bc90591e8a5a1c4
p23zadroznyw <p23zadroznyw@zsi.kielce.pl>
2024-11-28 16:45:24 +0100

    zdanie zrobione pieknie

---
+++ Stozek/index.php
@@ -0,0 +1,55 @@
+<?php
+    include_once "stozek.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <style>
+        body {
+            background-color: lightpink;
+            color: darkmagenta;
+            font-size: 50px;
+            padding-top: 100px;
+            padding-bottom: 100px;
+            padding-left: 0px;
+            padding-right: 0px;
+            text-align: center;
+            font-family: Consolas;
+        }
+
+        div {
+            border-top: solid;
+            border-bottom: solid;
+            border-right: none;
+            border-left: none;
+            border-width: 3px;
+            border-color: magenta;
+            display: inline-block;
+        }
+    </style>
+
+</head>
+<body>
+
+    <?php
+        $R = 5;
+        $H = 50;
+        $obj = new Stozek($R, $H);
+    ?>
+
+
+    <div>
+        <p>Stożek</p>
+        <p>Promień: R = <?=$R?> m</p>
+        <p>Wysokość: H = <?=$H?> m</p>
+        <p>Pole: P = <?=number_format($obj->poleCalkowite(), 2, ",", " ")?> m<sup>2</sup></p>
+        <p>Objętość: V = <?=number_format($obj->objetosc(), 2, ",", " ")?> m<sup>3</sup></p>
+    </div>
+</body>
+</html>
---
+++ Stozek/przeciwprostokatna.php
@@ -0,0 +1,8 @@
+<?php
+
+class PrzeciwProstokatna {
+    protected function ppk($a, $b): float {
+        return sqrt($a * $a + $b * $b);
+    }
+}
+
